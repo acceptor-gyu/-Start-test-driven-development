4dbf0390b4729aca18b069ca36684956afbe8c41
acceptor-gyu <gseonggyu968@gmail.com>
2022-11-21 22:04:07 +0900

    test code refactoring v1

--- TDD-test-chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ TDD-test-chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -6,19 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
     @Test
     void meetsAllCriteria_Then_Strong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@AB");
         assertEquals(PasswordStrength.STRONG, result);
     }
 
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@A");
         assertEquals(PasswordStrength.NORMAL, result);
         PasswordStrength result2 = meter.meter("Ab12!c");
         assertEquals(PasswordStrength.NORMAL, result2);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
     }
 }
